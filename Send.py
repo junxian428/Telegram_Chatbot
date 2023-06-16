f920153f4fb93974291445fe89a7f8ad353021d1
junxian428 <58724748+junxian428@users.noreply.github.com>
2023-06-16 10:56:31 +0800

    Update Send.py

--- Send.py
+++ Send.py
@@ -1,11 +1,14 @@
 import requests
 
 bot_token = ''
-chat_id = ''
-notification_text = 'Your notification message'
+chat_id = '-'
+message_text = 'my sample text'
 
-url = f'https://api.telegram.org/bot{bot_token}/sendMessage?chat_id={chat_id}&text={notification_text}'
+url = f'https://api.telegram.org/bot{bot_token}/sendMessage'
+params = {
+    'chat_id': chat_id,
+    'text': message_text
+}
 
-response = requests.get(url)
-
+response = requests.post(url, params=params)
 print(response.json())
